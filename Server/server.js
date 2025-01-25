8c851727448268083225bbc7461f6bd3318d6b98
Zkyarpan <karkiarpan555@gmail.com>
2025-01-25 22:20:21 +0545

    Add messaging feature with Socket.IO: implement message schema, configure Socket.IO server, and handle message sending and receiving.

--- Server/server.js
+++ Server/server.js
@@ -1,12 +1,46 @@
 import http from "http";
+import { Server } from "socket.io";
 import app from "./src/app.js";
 import config from "./src/config/config.js";
 import connectDB from "./src/config/db.js";
+import Message from "./src/models/Message.js";
 
 const startServer = async () => {
   try {
     await connectDB();
     const server = http.createServer(app);
+
+    const io = new Server(server, {
+      cors: {
+        origin: " https://255e-103-95-17-75.ngrok-free.app ",
+        methods: ["GET", "POST"],
+      },
+    });
+
+    io.on("connection", (socket) => {
+      socket.on("join_room", (roomId) => {
+        socket.join(roomId);
+      });
+
+      socket.on("send_message", async (data) => {
+        try {
+          const message = new Message({
+            sender: data.senderId,
+            receiver: data.receiverId,
+            content: data.message,
+          });
+          await message.save();
+
+          socket.to(data.room).emit("receive_message", data);
+        } catch (error) {
+          console.error("Error saving message:", error);
+        }
+      });
+
+      socket.on("disconnect", () => {
+        console.log("User disconnected");
+      });
+    });
 
     server.listen(config.port, async () => {
       console.log(`✅ Server: http://localhost:${config.port}`);
--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import Sidebar from '../components/Sidebar';
-import Topbar from '../components/Topbar';
-
+import React from "react";
+import Sidebar from "../components/Sidebar";
+import Topbar from "../components/Topbar";
 
 const Chat = () => {
   return (
     <div className="flex gap-1 bg-gray-50">
-    <Sidebar />
-    <div className="flex flex-col">
-      <Topbar />
+      <Sidebar />
+      <div className="flex flex-col">
+        <Topbar />
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
 export default Chat;
